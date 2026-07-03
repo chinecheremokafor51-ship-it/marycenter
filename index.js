2b093171cfc257f73b609971c337469a3325ffbe
Emmanuel <chinecheremokafor51@gmail.com>
2026-07-03 12:31:10 +0100

    first commit

---
+++ index.js
@@ -0,0 +1,8 @@
+
+
+const hamburger = document.getElementById("hamburger");
+const navMenu = document.getElementById("navMenu");
+
+hamburger.addEventListener("click", function () {
+    navMenu.classList.toggle("active");
+});
